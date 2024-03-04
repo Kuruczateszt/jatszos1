6dae7ff160fdfb528382345da6846befebe1e24b
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-04 14:24:31 +0100

    teszt

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+
 builder.Services.AddDbContext<Wshop3Context>();
 
 var app = builder.Build();
@@ -39,6 +40,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapControllers();
 
 app.Run();
 

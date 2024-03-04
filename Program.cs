12cb6e1205437d4b4dc9bbae01fb2d37c08b6a2a
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-04 09:51:59 +0100

    controllerek és controllers service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using wshop3.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Wshop3Context>();
 
 var app = builder.Build();
 
@@ -23,7 +27,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),

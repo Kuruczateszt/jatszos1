6f4581922a7d78b72bdcb76a9317812e184e00fa
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-04 15:37:48 +0100

    teszt

---
+++ Dto/FelhasznaloBejelentkezesDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wshop3.Dto
+{
+    public class FelhasznaloBejelentkezesDto
+    {
+        public string Nev { get; set; } = null!;
+
+        public string Jelszo { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}

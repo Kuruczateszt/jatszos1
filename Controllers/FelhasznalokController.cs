89a1493fc62f64d8b2a8b3a05509b2d62c81386d
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-04 13:15:31 +0100

    Felhasznalokcontroller felhasznaloid

--- Controllers/FelhasznalokController.cs
+++ Controllers/FelhasznalokController.cs
@@ -2,11 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wshop3.Model;
 
 namespace wshop3.Controllers
 {
-    public class FelhasznalokController
+    [ApiController]
+    [Route("[controller]")]
+    public class FelhasznalokController : ControllerBase
     {
-        
+        private readonly Wshop3Context _whop3Context;
+        public FelhasznalokController(Wshop3Context whop3Context)
+        {
+            _whop3Context = whop3Context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult FelhasznaloId([FromRoute] int id)
+        {
+            var felhasznalo = _whop3Context.Felhasznaloks.FirstOrDefault(f => f.Id == id);
+            if (felhasznalo == null)
+            {
+                return BadRequest("Nincs ilyen felhasználó");
+            }
+            return Ok(felhasznalo);
+        }
     }
 }

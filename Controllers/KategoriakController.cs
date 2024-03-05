8a60449300be4ec52f8894d363da9bb8a02aef13
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-05 15:05:23 +0100

    kategoriakcontroller

--- Controllers/KategoriakController.cs
+++ Controllers/KategoriakController.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wshop3.Model;
 
 namespace wshop3.Controllers
 {
-    public class KategoriakController
+    [ApiController]
+    public class KategoriakController : ControllerBase
     {
-        
+        private readonly Wshop3Context _whop3c;
+        public KategoriakController(Wshop3Context whop3Context)
+        {
+            _whop3c = whop3Context;
+        }
     }
 }

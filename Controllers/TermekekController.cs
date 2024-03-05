f2c7614b380e5679fe7cc6ca2ff4f629c58bc45c
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-05 15:04:28 +0100

    context nev mod

--- Controllers/TermekekController.cs
+++ Controllers/TermekekController.cs
@@ -12,16 +12,16 @@ namespace wshop3.Controller
     [Route("api/[controller]")]
     public class TermekekController : ControllerBase
     {
-        private readonly Wshop3Context _whop3Context;
+        private readonly Wshop3Context _whop3c;
         public TermekekController(Wshop3Context whop3Context)
         {
-            _whop3Context = whop3Context;
+            _whop3c = whop3Context;
         }
 
         [HttpGet("{id}")]
         public IActionResult TermekekId([FromRoute] int id)
         {
-            var termek = _whop3Context.Termekeks.Include(t => t.Kategoria).Include(t => t.TermekKep).FirstOrDefault(t => t.Id == id);
+            var termek = _whop3c.Termekeks.Include(t => t.Kategoria).Include(t => t.TermekKep).FirstOrDefault(t => t.Id == id);
             if (termek == null)
             {
                 return BadRequest("Nincs ilyen termek");
@@ -32,7 +32,7 @@ namespace wshop3.Controller
         [HttpGet]
         public IActionResult OsszesTermek()
         {
-            var termekek = _whop3Context.Termekeks.Include(t => t.Kategoria).Include(t => t.TermekKep);
+            var termekek = _whop3c.Termekeks.Include(t => t.Kategoria).Include(t => t.TermekKep);
             if (termekek.Count() == 0)
             {
                 return BadRequest("Nincsenek termekek");
@@ -43,13 +43,13 @@ namespace wshop3.Controller
         [HttpDelete("id")]
         public IActionResult TermekTorles([FromRoute] int id)
         {
-            var termek = _whop3Context.Termekeks.FirstOrDefault(t => t.Id == id);
+            var termek = _whop3c.Termekeks.FirstOrDefault(t => t.Id == id);
             if (termek == null)
             {
                 return BadRequest("Nincs ilyen termek");
             }
-            _whop3Context.Termekeks.Remove(termek);
-            _whop3Context.SaveChanges();
+            _whop3c.Termekeks.Remove(termek);
+            _whop3c.SaveChanges();
             return Ok();
         }
 

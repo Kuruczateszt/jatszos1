289629b284544002adb1e1fc4ab7c5f9112b8ff9
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-06 13:27:01 +0100

    Rendelesek controller

--- Controllers/RendelesekController.cs
+++ Controllers/RendelesekController.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wshop3.Model;
 
 namespace wshop3.Controllers
 {
-    public class RendelesekController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RendelesekController : ControllerBase
     {
-        
+        private readonly Wshop3Context _ws3;
+        public RendelesekController(Wshop3Context wshop3Context)
+        {
+            _ws3 = wshop3Context;
+        }
+
+        [HttpGet]
+        public IActionResult OsszesRendelesek()
+        {
+            var rendelesek = _ws3.Rendeleseks;
+
+            if (rendelesek.Count() == 0)
+            {
+                return BadRequest("Nincsenek rendelesek");
+            }
+
+            return Ok(rendelesek);
+
+        }
     }
 }

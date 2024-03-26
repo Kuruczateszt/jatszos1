8782cd05ae9dc7267023e1e7ba01eba9ba56d3e4
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-26 12:31:10 +0100

    új migrate

---
+++ Migrations/20240326113054_rendeles_ideje.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wshop3.Migrations
+{
+    /// <inheritdoc />
+    public partial class rendeles_ideje : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "rendeles_ideje",
+                table: "rendelesek",
+                type: "timestamp",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "rendeles_ideje",
+                table: "rendelesek");
+        }
+    }
+}

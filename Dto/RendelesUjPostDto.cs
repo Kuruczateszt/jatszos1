ed80d0ae5439453f39656244f2ca686c0fc8176b
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-26 14:07:40 +0100

    RendelesUjPostDto

---
+++ Dto/RendelesUjPostDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wshop3.Dto
+{
+    public class RendelesUjPostDto
+    {
+        public int? FelhasznaloId { get; set; }
+
+        public int? TermekId { get; set; }
+
+        public int? Mennyiseg { get; set; }
+    }
+}

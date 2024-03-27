a6e894244794fb4ca14112a9e75886c8ea78c78c
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-03-27 14:53:27 +0100

    új migrate

---
+++ Migrations/20240327135301_rendeles_termek3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wshop3.Migrations
+{
+    /// <inheritdoc />
+    public partial class rendeles_termek3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "rendelesek_ibfk_1",
+                table: "rendelesek");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "rendelesek_ibfk_1",
+                table: "rendelesek",
+                column: "felhasznalo_id",
+                principalTable: "felhasznalok",
+                principalColumn: "id");
+        }
+    }
+}

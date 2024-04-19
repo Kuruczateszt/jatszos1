dab50627babb79db1b650e180ca7d45476a43312
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-19 09:12:05 +0200

    osztály termék kereséshez

---
+++ filt/Fobj.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wshop3.filt
+{
+    public class Szures
+    {
+        public string? Keres { get; set; } = null;
+    }
+}

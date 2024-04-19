5df02f5dceebb9df4a3ad941f58d45dc0d41a767
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-19 14:21:27 +0200

    TermekekListAsync repo

--- Datab/repo/ITermekekRepo.cs
+++ Datab/repo/ITermekekRepo.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using wshop3.Datab;
+using wshop3.Dto;
+using wshop3.DtoMap;
+using wshop3.filt;
 using wshop3.Model;
 
 namespace wshop3.Datab.repo
@@ -23,5 +26,43 @@ namespace wshop3.Datab.repo
             return termek;
         }
 
+        public async Task<List<TermekOsszesLekerdezDto>> TermekListAsync(Szures szur)
+        {
+            var termekek = _ws3.Termekeks
+                .Include(t => t.Kategoria)
+                .Include(t => t.TermekKep)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(szur.Keres))
+            {
+                termekek = termekek.Where(t => t.Nev.Contains(szur.Keres) || (t.Leiras != null && t.Leiras.Contains(szur.Keres)));
+            }
+
+            if (!string.IsNullOrWhiteSpace(szur.Rendez))
+            {
+                //hibásan megadott érték esetén nincs rendezés
+                switch (szur.Rendez.ToLower())
+                {
+                    case "nev":
+                        termekek = szur.CsokkenoSorrend ?
+                        termekek.OrderByDescending(t => t.Nev) : termekek.OrderBy(t => t.Nev);
+                        break;
+                    case "ar":
+                        termekek = szur.CsokkenoSorrend ?
+                        termekek.OrderByDescending(t => t.Ar) : termekek.OrderBy(t => t.Ar);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //első oldalon nincs mit kihagyni, ezért a -1
+            var kihagy = (szur.Lapszam - 1) * szur.Lapmeret;
+            termekek = termekek.Skip(kihagy).Take(szur.Lapmeret);
+
+            var termekekvissza = await termekek.Select(t => t.TermekLekerdezDto()).ToListAsync();
+
+            return termekekvissza;
+        }
     }
 }

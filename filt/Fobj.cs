eb3999b8aff87da71a9304890a129d92f99515c8
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-19 09:31:40 +0200

    rendezéshez prop + comment

--- filt/Fobj.cs
+++ filt/Fobj.cs
@@ -7,6 +7,10 @@ namespace wshop3.filt
 {
     public class Szures
     {
+        //rendez + irány
+        public string? Rendez { get; set; } = null;
+        public bool CsokkenoSorrend { get; set; } = false;
+        //kereső szó
         public string? Keres { get; set; } = null;
     }
 }

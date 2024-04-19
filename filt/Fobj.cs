e810a21e74ebc00c09acd499b45223ca443cdefa
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-19 10:31:03 +0200

    új változók a tördeléshez

--- filt/Fobj.cs
+++ filt/Fobj.cs
@@ -12,5 +12,10 @@ namespace wshop3.filt
         public bool CsokkenoSorrend { get; set; } = false;
         //kereső szó
         public string? Keres { get; set; } = null;
+        //tördeléshez
+        //ha nincs megadva, mindig az első oldal jelenik meg
+        public int Lapszam { get; set; } = 1;
+        //egyszerre megjelenő termékek száma.
+        public int Lapmeret { get; set; } = 10;
     }
 }

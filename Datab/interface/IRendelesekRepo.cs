7204ce97fe65ec38b737d9bca9302c77e01a9320
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-20 17:33:55 +0200

    interface IRendelesekRepo

---
+++ Datab/interface/IRendelesekRepo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wshop3.Model;
+
+namespace wshop3.Datab
+{
+    public interface IRendelesekRepo
+    {
+        Task<Rendelesek?> RendelesekIdAsync(int id);
+        Task<List<Rendelesek>> OsszesRendelesekAsync();
+        Task<Rendelesek> RendelesekUjAsync(Rendelesek rendeles);
+    }
+}

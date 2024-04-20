fdbcdaa8cc561457bf853378504b9926d93924ca
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-20 15:54:23 +0200

    Kategoria interface + ITermekekIdAsync

---
+++ Datab/interface/IKategoriakRepo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wshop3.Model;
+
+namespace wshop3.Datab
+{
+    public interface IKategoriakRepo
+    {
+        Task<Kategoriak?> TermekekIdAsync(int id);
+    }
+}

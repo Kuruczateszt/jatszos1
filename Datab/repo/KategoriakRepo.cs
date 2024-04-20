cae51a504b06a399fdc50112cb60b9e6312da44c
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-20 15:59:41 +0200

    kategoria repo + TermekekIdAsync

---
+++ Datab/repo/KategoriakRepo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using wshop3.Model;
+
+namespace wshop3.Datab.repo
+{
+    public class KategoriakRepo : IKategoriakRepo
+    {
+        private readonly Wshop3Context _ws3;
+        public KategoriakRepo(Wshop3Context whop3Context)
+        {
+            _ws3 = whop3Context;
+        }
+        public async Task<Kategoriak?> TermekekIdAsync(int id)
+        {
+            var kategoria = await _ws3.Kategoriaks.FirstOrDefaultAsync(k => k.Id == id);
+
+            return kategoria;
+        }
+    }
+}

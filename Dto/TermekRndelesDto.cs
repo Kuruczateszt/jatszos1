6d82ce6a3bddffb6248a603768237fc37f1c1857
kurucz Attila <kurucz.attila@prtelecom.hu>
2024-04-21 09:55:41 +0200

    termekjson -> TermekRendelesDto

---
+++ Dto/TermekRndelesDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wshop3.Dto
+{
+    public class TermekRndelesDto
+    {
+        public int Id { get; set; }
+        public int Mennyiseg { get; set; }
+    }
+}
